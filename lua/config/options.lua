f76a86281cfde65217e91ac171583b9c52c8d32e
AM.A <janipooraminallah@gmail.com>
2025-05-18 13:16:51 +0330

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.backspace = "2"
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd([[ set noswapfile ]])
+vim.cmd([[ set termguicolors ]])
+
+--Line numbers
+vim.wo.number = true
+
+vim.opt.relativenumber = true
