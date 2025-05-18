1ea84bc303281efa7fbacf84ca81754e3c97a5fa
AM.A <janipooraminallah@gmail.com>
2025-05-18 13:39:10 +0330

    set autoformat = false for gem5

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    if cwd:match("gem5") then
+      vim.b.autoformat = false
+    end
+  end,
+})
