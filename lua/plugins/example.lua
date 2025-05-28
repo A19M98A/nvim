a49bfbe5d622ebf8538b7903cf51dcd6462d8c41
AM.A <janipooraminallah@gmail.com>
2025-05-29 01:36:42 +0330

    update lazy docker

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,26 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+    {
+  "mgierada/lazydocker.nvim",
+  dependencies = { "akinsho/toggleterm.nvim" },
+  config = function()
+    require("lazydocker").setup({
+	    border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+    })
+  end,
+  event = "BufRead",
+  keys = {
+    {
+      "<leader>ld",
+      function()
+        require("lazydocker").open()
+      end,
+      desc = "Open Lazydocker floating window",
+    },
+  },
+},
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -201,6 +221,25 @@ return {
       picker = {
         hidden = true,
         ignored = true,
+      },
+    },
+  },
+  {
+    "mgierada/lazydocker.nvim",
+    dependencies = { "akinsho/toggleterm.nvim" },
+    config = function()
+      require("lazydocker").setup({
+        border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+      })
+    end,
+    event = "BufRead",
+    keys = {
+      {
+        "<leader>ld",
+        function()
+          require("lazydocker").open()
+        end,
+        desc = "Open Lazydocker floating window",
       },
     },
   },
